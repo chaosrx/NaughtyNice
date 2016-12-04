53a921d88ec752145f30d1c3160250b7491311fb
Joseph Magdalena <joemagiv@gmail.com>
2016-12-04 13:29:44 -0800

    Basic loop completed. Need to start adding art and generation transition.

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 	
@@ -7,8 +8,17 @@ public class GameController : MonoBehaviour {
 	private Kid kid;
 	private PlayerValues playerValues;
 	
+	public int kidsServed = 0;
+	
+	public bool failureTriggered = false;
+	
+	public Button debugResetButton;
+	
+	public GameObject naughtyNiceButtons;
+	
 	// Use this for initialization
 	void Start () {
+		debugResetButton.gameObject.SetActive(false);
 		kidValues = FindObjectOfType<KidValues>().GetComponent<KidValues>();
 		kid = FindObjectOfType<Kid>().GetComponent<Kid>();
 		playerValues = FindObjectOfType<PlayerValues>().GetComponent<PlayerValues>();
@@ -24,21 +34,40 @@ public class GameController : MonoBehaviour {
 	}
 	
 	public void Naughty(){
-		int debugCheerReduction = Mathf.CeilToInt(kid.believerYears*kid.naughtyNiceLevel/kid.giftLevel);
+		int debugCheerReduction = Mathf.CeilToInt((kid.naughtyNiceLevel/kid.giftLevel) * kid.believerYears);
 		Debug.Log("Cheer reduction by: " + debugCheerReduction);
 		playerValues.cheer -= Mathf.CeilToInt((kid.believerYears/kid.naughtyNiceLevel) * kid.giftLevel * 2f);
 		playerValues.budget += kid.giftLevel * 2;
+		kidsServed++;
 		RandomizeKid();
 	}
 	
 	public void Nice(){
 		playerValues.cheer += Mathf.CeilToInt((kid.believerYears*kid.naughtyNiceLevel)/2);
 		playerValues.budget -= kid.giftLevel * 2;
+		kidsServed++;
 		RandomizeKid();
+	}
+	
+	public void DebugReset(){
+		//probably going to replace this with more robust logic once I start polishing
+		failureTriggered = false;
+		naughtyNiceButtons.gameObject.SetActive(true);
+		debugResetButton.gameObject.SetActive(false);
+		
+		kidsServed = 0;
+		playerValues.cheer = 75;
+		playerValues.budget = 75;
+		RandomizeKid();
+		
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (failureTriggered){
+			debugResetButton.gameObject.SetActive(true);
+			naughtyNiceButtons.gameObject.SetActive(false);
+		}
 	}
 }
--- Assets/PlayerValues.cs
+++ Assets/PlayerValues.cs
@@ -9,29 +9,41 @@ public class PlayerValues : MonoBehaviour {
 	public float budget;
 	
 	public Text playerValuesText;
-	
+	public Text kidText;
+	public GameController gameController;
 	
 	// Use this for initialization
 	void Start () {
+		gameController = FindObjectOfType<GameController>().GetComponent<GameController>();
+	}
 	
+	void UpdateKidText(){
+		kidText.text = "Game Over \n You judged " + gameController.kidsServed.ToString() + " kids \n Merry Christmas!";
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (cheer > 100){
 			playerValuesText.text = "Overcheer Failure Triggered";
-			
+			gameController.failureTriggered = true;
+			UpdateKidText();
 		} else {
 			if (cheer < 0){
 				playerValuesText.text = "Undercheer Failure Triggered";
+				gameController.failureTriggered = true;
+				UpdateKidText();
 			} else {
 				if (budget > 100){
 					playerValuesText.text = "Overbudget Failure Triggered";
+					gameController.failureTriggered = true;
+					UpdateKidText();
 				} else {
 					playerValuesText.text = "Cheer: " + cheer.ToString() + "\nWorkshop Magic: " + budget.ToString();
 					
 					if (budget < 0){
 						playerValuesText.text = "Underbudget Failure Triggered";
+						gameController.failureTriggered = true;
+						UpdateKidText();
 					}
 				}
 			}
