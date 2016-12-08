290c7b1962452dcb1c12aad4c07d1575bce5dead
joemag <joemagiv@gmail.com>
2016-12-08 11:44:37 -0800

    Added early intro scene work and dialogue manager.

---
+++ Assets/DebugIntro.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using PixelCrushers.DialogueSystem;
+
+public class DebugIntro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		DialogueManager.StartConversation ("TestConversation01");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
