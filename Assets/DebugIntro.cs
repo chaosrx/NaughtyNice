97ce8532bfce7ffe81be07e9b71697788765cd4e
joemag <joemagiv@gmail.com>
2016-12-18 17:36:35 -0800

    Effectively done, moving on to platform work.

--- Assets/DebugIntro.cs
+++ Assets/DebugIntro.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 using PixelCrushers.DialogueSystem;
+using UnityEngine.SceneManagement;
 
 public class DebugIntro : MonoBehaviour {
+	
+	public GameObject dialogueManager;
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +15,11 @@ public class DebugIntro : MonoBehaviour {
 	void GameStart(){
 		DialogueManager.StartConversation ("TestConversation01");
 		
+	}
+	
+	public void LoadMainGame(){
+		SceneManager.LoadScene ("scene01", LoadSceneMode.Single);
+		Destroy(dialogueManager);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/TitleCardController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TitleCardController : MonoBehaviour {
+	
+	public GameObject UIcanvas;
+	
+	// Use this for initialization
+	void Start () {
+		UIcanvas.SetActive(false);
+		Invoke("EnableUI",22f);
+	}
+	
+	void EnableUI(){
+		UIcanvas.SetActive(true);
+		
+	}
+	
+	public void LoadIntroLevel(){
+		SceneManager.LoadScene ("IntroScene",LoadSceneMode.Single);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
